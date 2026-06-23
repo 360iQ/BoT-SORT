cc102d2063b0cf0bfe6a0999d6d2ae0134089e6a
bmaciolek <bmaciolek@dtiq.com>
2026-06-23 15:56:08 +0200

    req adjustment

--- setup.py
+++ setup.py
@@ -16,6 +16,13 @@ assert torch_ver >= [1, 3], "Requires PyTorch >= 1.3"
 with open("README.md", "r") as f:
     long_description = f.read()
 
+def get_install_requirements():
+    with open("requirements.txt", "r", encoding="utf-8") as f:
+        reqs = [x.strip() for x in f.read().splitlines()]
+    reqs = [x for x in reqs if not x.startswith("#")]
+    return reqs
+
+
 def get_package_dir():
     pkg_dir = {
         "tracker": "tracker",
